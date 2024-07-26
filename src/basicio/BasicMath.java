cc88c12a861a8933554ba8316caa9a25ebe19774
kvlsnehankita <snehankita26@gmail.com>
2024-07-26 18:31:30 +0530

    basicmath program is added

---
+++ src/basicio/BasicMath.java
@@ -0,0 +1,23 @@
+package basicio;
+
+import java.util.Scanner;
+
+public class BasicMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("enter a number:");
+		int num = scanner.nextInt();
+
+		float square = num * num;
+		System.out.println("suare of the given number:" + square);
+
+		System.out.println("square root of given number:" + Math.sqrt(num));
+
+		scanner.close();
+
+	}
+
+}
